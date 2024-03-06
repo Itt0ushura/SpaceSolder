b40d64655f258d754d210cef2357d59daf8f2958
Itt0ushura <balashov580@gmail.com>
2024-03-06 19:52:15 +0200

    camera follow

---
+++ Assets/_Game/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera _camera;
+
+    [SerializeField] private Transform playerTransform;
+
+    [Space]
+    [SerializeField] private Vector3 cameraOffset;
+
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = playerTransform.position + playerTransform.right * cameraOffset.x + playerTransform.up * cameraOffset.y - playerTransform.forward * cameraOffset.z;
+
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, targetPosition, 0.1f);
+
+        _camera.transform.rotation = Quaternion.Lerp(_camera.transform.rotation, playerTransform.transform.rotation, 0.1f);
+    }
+}
